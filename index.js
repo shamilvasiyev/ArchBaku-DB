4f619e49989424c97de23b51dd44d1b28e378887
shamilvasiyev <shamilvasiyev@gmail.com>
2023-12-16 01:41:23 +0400

    file path has changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const fs = require("fs");
 const path = require("path");
-const filePath = path.join("/tmp", "db.json"); // Use __dirname to get the current directory
+const filePath = path.join(process.cwd(), "db.json"); // Use __dirname to get the current directory
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 
