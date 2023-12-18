3ce113ffb3f279d39bcdea86c366efbe05bfec42
shamilvasiyev <shamilvasiyev@gmail.com>
2023-12-18 20:59:11 +0400

    some bugs fixed

--- api/index.js
+++ api/index.js
@@ -2,39 +2,33 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const fs = require("fs");
 const path = require("path");
+const filePath = path.join(process.cwd(), "db.json"); // Use __dirname to get the current directory
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
 
-const filePath = path.join("/var/task", "db.json"); // Use /tmp for write operations in Vercel
+server.use(jsonServer.defaults());
+server.use(jsonServer.bodyParser); // Add this line to parse POST request body
 
-// Middleware to parse JSON bodies in POST requests
-server.use(jsonServer.bodyParser);
-
-// Use this line instead to enable write operations
-const router = jsonServer.router(filePath);
-
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-
-// Custom route to handle POST requests and save data to db.json
 server.post("/api/data", (req, res) => {
-  // Assuming that req.body contains the data you want to save
-  router.db.data.push(req.body);
-  fs.writeFileSync(filePath, JSON.stringify(router.db, null, 2));
-  res.status(200).json({ message: "Data saved successfully" });
+  // Assuming the POST request contains data in the body
+  const newData = req.body;
+
+  // Update your db object with the new data
+  // For example, let's assume you have an array named 'items' in your db.json
+  db.items.push(newData);
+
+  // Save the updated data back to the db.json file
+  fs.writeFileSync(filePath, JSON.stringify(db, null, 2));
+
+  // Respond with the new data
+  res.json(newData);
 });
 
+// Other routes and configurations...
+const router = jsonServer.router(filePath);
 server.use(router);
 
-// The following code is necessary to make it work on Vercel
-module.exports = (req, res) => {
-  // Allow CORS (Cross-Origin Resource Sharing)
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-
-  server(req, res);
-};
+const port = 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
